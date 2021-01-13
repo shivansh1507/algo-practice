8ce9e7c290017559003ea2ddf7eb4f1da3b9efaf
shivansh1507 <74969179+shivansh1507@users.noreply.github.com>
2021-01-13 13:12:44 +0530

    temp commit

--- java/ac.java
+++ java/ac.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class ac{
     public static void main(String args[]){
-        int [] a1={1,2,3};
+        int [] a1={2,2,3};
         int [] a2={4,5,6};
         int length=a1.length+a2.length;
         int [] ac=new int[length];
